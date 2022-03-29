e4105e8f30f400908ff785033a882ff0a78f514e
RYDanielRY <ry.daniil.ry@yandex.ru>
2022-03-29 12:11:01 +0200

    ButtonClick

--- h1.js
+++ h1.js
@@ -1,11 +1,13 @@
 window.onload = init; 
 
-function init ()
+function init()
 {
-	var button = document.getElementById("Click Me"); 
-    button.onclick = handleButtonClick; 
+	var button = document.getElementById("Button"); 
+    button.onclick = handClick; 
 }
 
-function handleButtonClick() {
-	alert("Button was clicked"); 
+function handClick() 
+{
+	var text = document.getElementById("hello"); 
+	text.innerHTML = "Button Clicked"; 
 }
